7c39618ca98f57c11d414c8ea87f1bc23d5a6018
Abel <upskill229@alunos.fc.ul.pt>
2026-01-28 14:41:13 +0000

    feat: projeto final

--- dist/src/renderers/renderUser.d.ts
+++
@@ -1,6 +0,0 @@
-import { UserService } from '../services/UserService.js';
-export declare class RenderUser {
-    private userService;
-    constructor(userService: UserService);
-    render(): void;
-}
